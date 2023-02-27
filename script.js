356858da8dbaa88a59e35bf1dd9d761cb6d30dee
safatsaat2 <sakibsafat47@gmail.com>
2023-02-27 13:52:46 +0600

    api link connected

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener('click', () => {
     let countryName = countryInp.value;
-    let finalUrl = ``;
+
+    let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+    console.log(finalUrl);
+    
+    fetch(finalUrl).then(res => res.json()).then(data => {console.log(data)})
 })
