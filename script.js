d3f3545be1a6c8718c564ec810cf28049fd39d57
safatsaat2 <sakibsafat47@gmail.com>
2023-02-27 15:37:14 +0600

    capital continent population currency added

--- script.js
+++ script.js
@@ -13,7 +13,31 @@ searchBtn.addEventListener('click', () => {
         console.log(data[0])
         result.innerHTML = `
         <img src="${data[0].flags.svg}" class= "flag-img">
-
+        <h2>${data[0].name.common}</h2>
+        <div class= "wrapper">
+            <div class= "data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class= "wrapper">
+            <div class= "data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+        <div class= "wrapper">
+            <div class= "data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class= "wrapper">
+            <div class= "data-wrapper">
+                <h4>Currency:</h4>
+                <span>${data[0].currencies[Object.keys(data[0].currencies)].name}</span>
+            </div>
+        </div>
         `
     })
 })
