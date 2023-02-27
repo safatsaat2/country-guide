ec59a76365a3cc966cee2ca5f2c7ca34919c2006
safatsaat2 <sakibsafat47@gmail.com>
2023-02-27 16:00:58 +0600

    full project done

--- script.js
+++ script.js
@@ -48,5 +48,12 @@ searchBtn.addEventListener('click', () => {
         </div>
         `
     })
+    .catch(() => {
+        if (countryName.length === 0){
+            result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+        } else{
+            result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+        }
+    })
 })
 
